ee6fd2983c92e034dcf5975eb0fd82ffddb2a5f1
Darren Buller <stash-taco-remake@duck.com>
2024-12-16 19:25:08 +0000

    Revert "Add tailwind and assets"
    This reverts commit 20d85a1ccad28879a3730e5ba2d30466c1ef3d89.

--- gym-react/src/App.tsx
+++ gym-react/src/App.tsx
@@ -2,11 +2,7 @@
 function App() {
 
   return (
-      <div className="app">
-        <h1 className="text-4xl text-red-900 font-bold underline">
-        Hello world!
-        </h1>
-      </div>
+      <div className="app">app</div>
   )
 }
 
