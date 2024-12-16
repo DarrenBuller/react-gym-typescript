20d85a1ccad28879a3730e5ba2d30466c1ef3d89
Darren Buller <stash-taco-remake@duck.com>
2024-12-16 19:07:56 +0000

    Add tailwind and assets

--- gym-react/src/App.tsx
+++ gym-react/src/App.tsx
@@ -2,7 +2,11 @@
 function App() {
 
   return (
-      <div className="app">app</div>
+      <div className="app">
+        <h1 className="text-4xl text-red-900 font-bold underline">
+        Hello world!
+        </h1>
+      </div>
   )
 }
 
